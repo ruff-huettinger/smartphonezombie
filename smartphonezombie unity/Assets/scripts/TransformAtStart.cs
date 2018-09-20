647f79aeaf07dcdf70ac559175d8646383cc8e5d
Khadivi <khadivi@huettinger.de>
2018-09-20 17:29:42 +0200

    add a new script "TransformAtStart" for camera from the gadget.

---
+++ smartphonezombie unity/Assets/scripts/TransformAtStart.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformAtStart : MonoBehaviour {
+    
+	// Use this for initialization
+	void Start () {
+        float x = (float)Convert.ToDouble(Configuration.GetAttricuteByTagName("camera_position", "x"));
+        float y = (float)Convert.ToDouble(Configuration.GetAttricuteByTagName("camera_position", "y"));
+        float z = (float)Convert.ToDouble(Configuration.GetAttricuteByTagName("camera_position", "z"));
+
+        float r = (float)Convert.ToDouble(Configuration.GetAttricuteByTagName("camera_rotation", "x"));
+
+        transform.localPosition = new Vector3(x, y, z);
+        transform.localEulerAngles = new Vector3(r, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
